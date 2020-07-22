0529f81ca17f8b85926659a220bc7b77be2fc47d
GeraldSolano <geraldmacias.gm@gmail.com>
2020-07-22 01:45:41 -0600

    promise to async await

--- src/services/getGif.js
+++ src/services/getGif.js
@@ -1,16 +1,12 @@
-
-export default function getGif ({keyword='morty'}={}) {
-    const API =`https://api.giphy.com/v1/gifs/search?api_key=MX9nJ74wU86uiJRPDx3C7Eid9iaEFyAF&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
-
-    return fetch(API)
-    .then((res) => res.json())
-    .then((response) => {
-      const { data } = response;
-      const gifs = data.map((image) =>{ 
-          const {images, title, id} = image
-          const {url} = images.downsized_medium
-          return {title, id, url}
-        });
-       return gifs;
-    });
+export default async function getGif({ keyword = "morty" } = {}) {
+  const API = `https://api.giphy.com/v1/gifs/search?api_key=MX9nJ74wU86uiJRPDx3C7Eid9iaEFyAF&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
+  const respuesta = await fetch(API);
+  const datax = await respuesta.json();
+  const { data } = datax;
+  const gifs = data.map((image) => {
+    const { images, title, id } = image;
+    const { url } = images.downsized_medium;
+    return { title, id, url };
+  });
+  return gifs;
 }
